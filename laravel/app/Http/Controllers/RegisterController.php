df5a6231a1b311531849baf4d0f7e87bff938889
savickiy2007 <mbmw34811@gmail.com>
2026-05-18 11:33:53 +0000

    b

---
+++ laravel/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RegisterController extends Controller
+{
+    public function show()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request ->validate([
+            "name" => "required|string|min:3",
+            "email" => "required|email|unique:users",
+            "password" => "required|string|min:8"
+
+        ]);
+
+        $user = User::create([
+            ...$data,
+            'password' => bcrypt($data['password'])
+        ]);
+
+        Auth::login($user);
+
+        return redirect()->route('dashboard');
+    }
+}
